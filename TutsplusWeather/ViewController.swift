5656fd7686d666fd5c9ad85eb30e766f669820ee
Markus Mühlberger <markus@mmuehlberger.com>
2017-01-31 15:58:58 +0100

    Initial Commit

---
+++ TutsplusWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TutsplusWeather
+//
+//  Created by Markus Mühlberger on 31/01/2017.
+//  Copyright © 2017 Markus Mühlberger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
